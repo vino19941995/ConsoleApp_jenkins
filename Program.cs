f057b8b15d50d82bb1f6a14d0f1146e34d9f2825
vino19941995 <vinodhinimohan1994@gmail.com>
2022-06-08 14:15:00 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp_jenkins
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
